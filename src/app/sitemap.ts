dc87f63a5ab24898bde4b289cd0415cde6a9dc07
zengzilin <zengzilin@users.noreply.github.com>
2024-10-01 00:04:02 +0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { POSTS } from "@/lib/constants";
 import { getBlogPosts } from "./blog/utils";
 
-export const baseUrl = "https://next-blog-cj.vercel.app";
+export const baseUrl = "https://gptzero.tools";
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
