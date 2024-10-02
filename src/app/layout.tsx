3f412116db2790105bb35d7ad3929ba006d5700d
zengzilin2016@gmail.com <zengzilin2016@gmail.com>
2024-10-02 19:54:24 +0800

    添加google ads代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/footer";
 import { siteConfig } from "@/config/site";
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -60,6 +61,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head><Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7977474508095040" crossOrigin="anonymous" /></head>
       <body className={inter.className}>
         <GoogleTagManager gtmId="G-9LQ118FGES" />
         <ThemeProvider
